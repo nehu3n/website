d576ea1b2071da8371887ccf98c265d6720a2300
nehu3n <developer.nh3@gmail.com>
2024-11-03 00:46:58 -0300

    ✨ feat(blog): Added the blog page.
    The routing, layout and configuration for the blog was done.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx(), sitemap()],
 });
