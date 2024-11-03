9f077b1b12559dd315a105867f2ed54c3725e20c
nehu3n <developer.nh3@gmail.com>
2024-11-02 23:37:55 -0300

    ✨ feat(portfolio): Added Layout, dark mode and main section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      maxWidth: {
+        content: "680px",
+      },
+    },
+  },
   plugins: [],
 };
